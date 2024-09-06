95597dba6de7efab5eca14123996d972ed6034a4
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-06 12:10:45 +0300

    write the function in the js for dynamic

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const containerE1 = document.querySelector(".container");
 
-const careers = ["YouTube", "WebDeveloper","Freelancer","Instructor"];
+const careers = ["YouTuber", "WebDeveloper", "Freelancer", "Instructor"];
 
-containerE1.innerHTML = `<h1>I am a ${careers}</h1>
-`;
+let carreerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+    characterIndex++;
+
+    containerE1.innerHTML = `<h1>I am a ${careers[carreerIndex].slice(0, characterIndex)}</h1>`;
+
+    if (characterIndex === careers[carreerIndex].length) {
+        carreerIndex++;
+        characterIndex = 0;
+    }
+    setTimeout(updateText, 400);
+}
+
